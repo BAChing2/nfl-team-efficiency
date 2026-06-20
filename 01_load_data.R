b3ed663cd3d5f2d4fe412fce36476f23d9c887f7
BAChing2 <brenden.j.ching@gmail.com>
2026-06-20 16:02:04 -0700

    Update 01_load_data.R
    Load play by play and compute offensive and defensive EPA per team.

--- 01_load_data.R
+++ 01_load_data.R
@@ -1,1 +1,15 @@
 #Load the data in 
+library(nflreadr)
+library(tidyverse)
+
+pbp <- load_pbp(2024)
+
+filtered_off_epa_pbp <- pbp %>%
+  filter(play_type %in% c("run", "pass")) %>%
+  group_by(posteam) %>% 
+  summarize(off_epa = mean(epa, na.rm = TRUE))
+
+filtered_def_epa_pbp <- pbp %>%
+  filter(play_type %in% c("run", "pass")) %>%
+  group_by(defteam) %>%
+  summarize(def_epa = mean(epa, na.rm = TRUE))

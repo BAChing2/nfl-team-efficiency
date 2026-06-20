e159a3b5400be6602eed9c9897359844e0e2f941
BAChing2 <brenden.j.ching@gmail.com>
2026-06-20 16:37:49 -0700

    Update 01_load_data.R

--- 01_load_data.R
+++ 01_load_data.R
@@ -17,3 +17,9 @@ filtered_def_epa_pbp <- pbp %>%
   summarize(def_epa = mean(epa, na.rm = TRUE))
 
 combined_epa <- full_join(filtered_off_epa_pbp, filtered_def_epa_pbp, by = join_by(team))
+
+ggplot(combined_epa, aes(x = off_epa, y = def_epa)) +
+  geom_point() +
+  scale_y_reverse()+
+  labs(title = "2024 NFL Team Efficiency: Offense vs Defense EPA per Play", y="Defensive EPA", x="Offensive EPA")
+ggsave("output/epa_quadrant_2024.png", width = 10, height = 8, dpi = 300)

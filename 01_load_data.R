36e80b8c7129f95425e479eef41cb8ef4df3867f
BAChing2 <brenden.j.ching@gmail.com>
2026-06-20 16:24:17 -0700

    Update 01_load_data.R

--- 01_load_data.R
+++ 01_load_data.R
@@ -5,11 +5,15 @@ library(tidyverse)
 pbp <- load_pbp(2024)
 
 filtered_off_epa_pbp <- pbp %>%
+  rename(team = posteam) %>%
   filter(play_type %in% c("run", "pass")) %>%
-  group_by(posteam) %>% 
+  group_by(team) %>% 
   summarize(off_epa = mean(epa, na.rm = TRUE))
 
 filtered_def_epa_pbp <- pbp %>%
+  rename(team = defteam) %>%
   filter(play_type %in% c("run", "pass")) %>%
-  group_by(defteam) %>%
+  group_by(team) %>%
   summarize(def_epa = mean(epa, na.rm = TRUE))
+
+combined_epa <- full_join(filtered_off_epa_pbp, filtered_def_epa_pbp, by = join_by(team))
